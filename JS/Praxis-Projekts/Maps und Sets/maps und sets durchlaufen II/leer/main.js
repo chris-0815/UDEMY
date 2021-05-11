211974bc1f06de14a34cc52f1f19f897adec3829
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2021-05-11 18:26:23 +0200

    ""

--- JS/Praxis-Projekts/Maps und Sets/maps und sets durchlaufen II/leer/main.js
+++ JS/Praxis-Projekts/Maps und Sets/maps und sets durchlaufen II/leer/main.js
@@ -3,8 +3,20 @@
 
 let meine_map = new Map();
 meine_map.set("Test", "Wert der Eigenschaft Test");
-meine_map.set("Test", "Wert der Eigenschaft Test");
-meine_map.set("Test", "Wert der Eigenschaft Test");
-meine_map.set("Test", "Wert der Eigenschaft Test");
-meine_map.set("Test", "Wert der Eigenschaft Test");
-meine_map.set("Test", "Wert der Eigenschaft Test");
+meine_map.set(13, "Wert der Eigenschaft 13");
+meine_map.set([], "Wert der Eigenschaft []");
+meine_map.set(function() {}, "Wert der Eigenschaft function");
+meine_map.set({}, "Wert der Eigenschaft {}");
+console.log(meine_map);
+
+let mein_set = new Set();
+mein_set.add("Test");
+mein_set.add("Test");
+mein_set.add([]);
+mein_set.add(function() {});
+mein_set.add({});
+console.log(mein_set);
+
+meine_map.entries();
+
+
