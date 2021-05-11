082dab2b86cd5987ff2f7caa385aabab1b68fda6
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2021-05-11 16:22:06 +0200

    ""

---
+++ JS/Praxis-Projekts/Maps und Sets/maps und sets durchlaufen II/leer/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+let meine_map = new Map();
+meine_map.set("Test", "Wert der Eigenschaft Test");
+meine_map.set("Test", "Wert der Eigenschaft Test");
+meine_map.set("Test", "Wert der Eigenschaft Test");
+meine_map.set("Test", "Wert der Eigenschaft Test");
+meine_map.set("Test", "Wert der Eigenschaft Test");
+meine_map.set("Test", "Wert der Eigenschaft Test");
