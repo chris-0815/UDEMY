06838f5926325e2f81b10753e79003060bac2e94
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2022-12-03 16:27:20 +0100

    Sets und maps durchlaufen 2

--- JS/Praxis-Projekts/Maps und Sets/maps und sets durchlaufen II/maps und sets durchlaufen II/main.js
+++ JS/Praxis-Projekts/Maps und Sets/maps und sets durchlaufen II/maps und sets durchlaufen II/main.js
@@ -17,10 +17,41 @@ mein_set.add(function() {});
 mein_set.add({});
 console.log(mein_set);
 
-meine_map.entries();
+
+// console.log(meine_map.entries());
+// console.log(meine_map.values());
+// console.log(meine_map.keys());
+// Über Maps iterieren
+for (let paar of meine_map.entries()) {
+    console.log(paar);
+}
+for (let [eigenschaft, wert] of meine_map.entries()) {
+    console.log(eigenschaft);
+    console.log(wert);
+}
+for (let eigenschaft of meine_map.keys()) {
+    console.log(eigenschaft);
+}
+for (let eigenschaft of meine_map.values()) {
+    console.log(eigenschaft);
+}
+// Über Sets iterieren
+for (let wert of mein_set.values()) {
+    console.log(wert);
+}
+
+// ??
 
 meine_map.forEach(function(wert, eigenschaft, map) {
     console.log(wert);
     console.log(eigenschaft);
     console.log();
 });
+
+let myElement = document.createElement('div');
+myElement.innerText = "servus du kleiner scheisser";
+document.body.appendChild(myElement);
+
+let btn = document.createElement('button');
+btn.innerHTML = "Hello Button";
+document.body.appendChild(btn);
