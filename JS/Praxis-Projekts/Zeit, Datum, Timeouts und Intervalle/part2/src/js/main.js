76cad20c0fd085115fec8bd12445e415079eb852
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2022-12-03 20:49:27 +0100

    date getter und setter

--- JS/Praxis-Projekts/Zeit, Datum, Timeouts und Intervalle/part2/src/js/main.js
+++ JS/Praxis-Projekts/Zeit, Datum, Timeouts und Intervalle/part2/src/js/main.js
@@ -3,6 +3,21 @@
 let d = new Date();
 
 d.setFullYear(1993);
-d.setMonth(0);
+d.setMonth(2);
+d.setDate(18);
+d.setHours(9);
+d.setMinutes(34);
+d.setSeconds(12);
+d.setMilliseconds(111);
+
+let c = new Date();
+c.getFullYear();
+c.getMonth();
+c.getDate();
+c.getHours();
+c.getMinutes();
+c.getSeconds();
+c.getMilliseconds();
 
 console.log(d);
+console.log(c);
