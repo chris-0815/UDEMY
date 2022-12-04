1fb263d521b18df76cfd49e0235708865ffceb69
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2022-12-04 15:53:57 +0100

    TimeObjekt Abruf Manipulation

--- JS/Praxis-Projekts/Zeit, Datum, Timeouts und Intervalle/part2/src/js/main.js
+++ JS/Praxis-Projekts/Zeit, Datum, Timeouts und Intervalle/part2/src/js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 let d = new Date();
-
 d.setFullYear(1993);
 d.setMonth(2);
 d.setDate(18);
@@ -11,13 +10,39 @@ d.setSeconds(12);
 d.setMilliseconds(111);
 
 let c = new Date();
-c.getFullYear();
-c.getMonth();
-c.getDate();
-c.getHours();
-c.getMinutes();
-c.getSeconds();
-c.getMilliseconds();
+console.log(c.getFullYear());
+console.log(c.getMonth());
+console.log(c.getDate());
+console.log(c.getHours());
+console.log(c.getMinutes());
+console.log(c.getSeconds());
+console.log(c.getMilliseconds());
 
 console.log(d);
 console.log(c);
+
+let d_utc = new Date();
+console.log(d_utc.setUTCFullYear(1993));
+console.log(d_utc.setUTCMonth(2));
+console.log(d_utc.setUTCDate(18));
+console.log(d_utc.setUTCHours(9));
+console.log(d_utc.setUTCMinutes(34));
+console.log(d_utc.setUTCSeconds(12));
+console.log(d_utc.setUTCMilliseconds(111));
+
+console.log(d_utc);
+
+let c_utc = new Date();
+console.log(c_utc.getUTCFullYear());
+console.log(c_utc.getUTCMonth());
+console.log(c_utc.getUTCDate());
+console.log(c_utc.getUTCHours());
+console.log(c_utc.getUTCMinutes());
+console.log(c_utc.getUTCSeconds());
+console.log(c_utc.getUTCMilliseconds());
+
+console.log(c_utc);
+
+d_unix.setTime(123546347345);
+console.log(d_unix);
+console.log(d_unix.getTime());
