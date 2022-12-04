08dd550cf4e1f38c086ba042461865167f208a8a
chris-0815 <58494290+chris-0815@users.noreply.github.com>
2022-12-04 18:02:06 +0100

    Methoden für Strings

--- JS/Praxis-Projekts/Arbeiten mit Strings und Numbers/Methoden für Numbers/main.js
+++ JS/Praxis-Projekts/Arbeiten mit Strings und Numbers/Methoden für Numbers/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let s = " Lorem ipsum dolor sit amet ipsum. ";
+
+console.log(s.length);
+// Achtung Case-Sensitive Funktionen
+console.log(s.indexOf("ipsum", 20));
+console.log(s.lastIndexOf("ipsum", 10));
+console.log(s.includes("sit", 28));
+console.log(s.toUpperCase());
--- JS/Praxis-Projekts/Arbeiten mit Strings und Numbers/Methoden für Strings/main.js
+++ JS/Praxis-Projekts/Arbeiten mit Strings und Numbers/Methoden für Strings/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let s = " Lorem ipsum dolor sit amet ipsum. ";
+
+console.log(s.length);
+// Achtung Case-Sensitive Funktionen
+console.log(s.indexOf("ipsum", 20));
+console.log(s.lastIndexOf("ipsum", 10));
+console.log(s.includes("sit", 28));
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.trim()); // entfernt leerzeichen am anfang und am ende
+console.log(s.repeat(100));
