7fe716892e4671cf7d8104ec2beb63481ebf0292
chris-0815 <christophbinner@gmail.com>
2022-12-07 17:52:01 +0100

    Gleitkommazahlen rechnen

---
+++ JS/Praxis-Projekts/Arbeiten mit Strings und Numbers/Gleitkommazahlen/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log(99.93 + 354.97); // 454.90000000000003 das loggt die Console.
+console.log(9332654729891549); // 9332654729891548 das loggt die Console.
+console.log((99.93 * 100 + 354.97 * 100) / 100); // 454.9 das loggt die Console.
+console.log(((99.93 * 100 + 354.97 * 100) / 100).toFixed(2)); // 454.9 das loggt die Console. Aber jetzt ist es ein String
