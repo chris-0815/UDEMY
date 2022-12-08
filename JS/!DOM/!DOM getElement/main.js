5a8a5db4389ce600295a04ceff7d6bfb2c574699
chris-0815 <christophbinner@gmail.com>
2022-12-08 17:58:19 +0100

    Document objekt model Übungen

---
+++ JS/!DOM/!DOM getElement/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let el_by_id_1 = document.getElementById("navigation");
+console.log(el_by_id_1);
+
+let el_by_id_2 = document.getElementById("test");
+console.log(el_by_id_2);
+
+let els_by_class_name_1 = document.getElementsByClassName("jumbotron");
+console.log(els_by_class_name_1);
+
+let els_by_class_name_2 = document.getElementsByClassName("test");
+console.log(els_by_class_name_2);
+
+//console.log(els_by_class_name_2.length == 0);
+
+let els_by_tag_name = document.getElementsByTagName("p");
+console.log(els_by_tag_name);
